50f4e23644cf00c56fb5e3b71205536108bfb22e
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2025-08-01 19:55:42 +0200

    navbar.php

--- app/Views/partials/base_components/navbar.php
+++ app/Views/partials/base_components/navbar.php
@@ -1,14 +1,11 @@
 <nav class="navbar navbar-expand-sm bg-body-tertiary mb-4 shadow">
     <div class="container-fluid align-content-center">
-        <a class="navbar-brand mb-0 h1 text-primary"
-           href="<?= base_url(index_page()) . "/home" ?>">F-WE</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <?php if ($view != 'login') { ?>
+        <a class="navbar-brand mb-0 h1 text-primary" href="<?= base_url(index_page()) . "/home" ?>">F-WE</a>
+        <?php if ($view != 'login') { ?>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto ">
                     <li class="nav-item">
                         <a class="active <?= $view == 'home' ? 'text-decoration-underline' : '' ?> nav-link" aria-current="page" href="<?= base_url(index_page()) . "/home" ?>">Startseite</a>
