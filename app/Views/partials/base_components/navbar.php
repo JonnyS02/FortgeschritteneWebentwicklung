a75ebcab923e4ff263676b8fb6d9f4b39de0e1db
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2025-08-01 19:18:34 +0200

    home.php

--- app/Views/partials/base_components/navbar.php
+++ app/Views/partials/base_components/navbar.php
@@ -9,29 +9,28 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <?php if ($view != 'login') { ?>
-            <ul class="navbar-nav me-auto ">
-                <li class="nav-item">
-                    <a class="active <?= $view == 'home' ? 'text-decoration-underline' : '' ?> nav-link"
-                       aria-current="page" href="<?= base_url(index_page()) . "/home" ?>">Startseite</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown-center">
-                    <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Konto
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li>
-                            <a class="dropdown-item interactive-navbar" href="<?= base_url(index_page()) . "/profile" ?>"><i class="fa-regular fa-circle-user" style="font-size: unset"></i> Max Mustermann</a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li>
-                            <a class="dropdown-item interactive-navbar" href="<?= base_url(index_page()) ?>"><i class="fa-solid fa-power-off" style="font-size: unset"></i> Abmelden</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+                <ul class="navbar-nav me-auto ">
+                    <li class="nav-item">
+                        <a class="active <?= $view == 'home' ? 'text-decoration-underline' : '' ?> nav-link"
+                           aria-current="page" href="<?= base_url(index_page()) . "/home" ?>">Startseite</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown-center">
+                        <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Konto</a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <a class="dropdown-item interactive-navbar" href="<?= base_url(index_page()) . "/profile" ?>"><i class="fa-regular fa-circle-user" style="font-size: unset"></i> Max Mustermann</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item interactive-navbar" href="<?= base_url(index_page()) ?>"><i class="fa-solid fa-power-off" style="font-size: unset"></i> Abmelden</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             <?php } ?>
         </div>
     </div>
