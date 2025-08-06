8ec84f6c0260106c7801691928f2b831cc1b2e5b
JonnyS02 <jona.stengl@gmail.com>
2025-08-06 14:18:56 +0200

    db-test

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,4 +1,9 @@
 <h2 class="text-center fw-light">
     Willkommen auf der Startseite!
 </h2>
+<ul>
+    <?php foreach ($test as $item): ?>
+        <li><?= $item['name'] ?></li>
+    <?php endforeach; ?>
+</ul>
 <?php var_dump($test); ?>
