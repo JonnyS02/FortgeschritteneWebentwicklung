38caf73a32b153eb6da4fb01d18441bd0a4179bc
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2025-08-07 18:12:58 +0200

    filter

--- app/Views/startseite.php
+++ app/Views/startseite.php
@@ -9,7 +9,7 @@
             </div>
         </div>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-6 mt-4">
         <div class="card shadow h-100">
             <div class="card-header">
                 <h6 class="display-6">Wetter von Trier</h6>
