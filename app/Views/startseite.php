2cf057030bdc6aad9e0250da251a86a87e1f7318
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2025-08-07 19:09:28 +0200

    filter

--- app/Views/startseite.php
+++ app/Views/startseite.php
@@ -5,47 +5,55 @@
                 <h6 class="display-6">Karte von Trier in Leaflet</h6>
             </div>
             <div class="card-body">
-                <div id="map" style="min-height: 600px" class="rounded"></div>
+                <div id="map"  class="rounded h-100"></div>
             </div>
         </div>
     </div>
     <div class="col-md-6">
         <div class="card shadow h-100 mt-md-0 mt-4">
             <div class="card-header">
-                <h6 class="display-6">Wetter von Trier</h6>
+                <h6 class="display-6">Wetter von OpenWeatherMap</h6>
             </div>
             <div class="card-body">
                 <div class="row">
                     <div class="col">
-                        <img src="https://openweathermap.org/img/wn/<?= $wetter['weather'][0]['icon'] ?>@2x.png" alt="Wetter-Icon">
+                        <div class="w-100"><img src="https://openweathermap.org/img/wn/<?= $wetter['weather'][0]['icon'] ?>@2x.png" alt="Wetter-Icon" class="w-50 w-md-75"></div>
+                            Das Wetter in Trier ist derzeit <h6 class="display-6"><?= $wetter['weather'][0]['description'] ?></h6> mit einer Temperatur von <h6 class="display-6"><?= $wetter['main']['temp'] ?>°C.</h6>
                     </div>
                     <div class="col">
-                        Das Wetter in Trier ist derzeit <b><?= $wetter['weather'][0]['description'] ?></b> mit einer Temperatur von <?= $wetter['main']['temp'] ?>°C.
                         <table class="table table-striped border mt-3">
                             <tbody>
                             <tr>
                                 <td>Temperatur</td>
-                                <td><?= $wetter['main']['temp'] ?></td>
+                                <td><?= $wetter['main']['temp'] ?> °C</td>
                             </tr>
                             <tr>
-                                <td>Gefühlte Temperatur</td>
-                                <td><?= $wetter['main']['feels_like'] ?></td>
+                                <td>Gefühlt</td>
+                                <td><?= $wetter['main']['feels_like'] ?> °C</td>
                             </tr>
                             <tr>
                                 <td>Tiefstwerte</td>
-                                <td><?= $wetter['main']['temp_min'] ?></td>
+                                <td><?= $wetter['main']['temp_min'] ?> °C</td>
                             </tr>
                             <tr>
                                 <td>Höchstwerte</td>
-                                <td><?= $wetter['main']['temp_max'] ?></td>
+                                <td><?= $wetter['main']['temp_max'] ?> °C</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <table class="table table-striped border mt-3">
+                            <tbody>
+                            <tr>
+                                <td>Luftfeuchtigkeit</td>
+                                <td><?= $wetter['main']['humidity'] ?> %</td>
                             </tr>
                             <tr>
-                                <td>Wind</td>
-                                <td><?= $wetter['wind']['speed'] ?></td>
+                                <td>Luftdruckk</td>
+                                <td><?= $wetter['main']['pressure'] ?> hPa</td>
                             </tr>
                             <tr>
-                                <td>Höchstwerte</td>
-                                <td><?= $wetter['main']['temp_max'] ?></td>
+                                <td>Windgeschwindigkeit</td>
+                                <td><?= $wetter['wind']['speed'] ?> m/s</td>
                             </tr>
                             </tbody>
                         </table>
