bff1d7e522af3ff527990f01d88d781afb56087e
JonnyS02 <jona.stengl@gmail.com>
2025-08-07 09:53:29 +0200

    db-test

--- app/Views/startseite.php
+++ app/Views/startseite.php
@@ -1,3 +1,22 @@
-<h1 class="text-center fw-light">
-    Willkommen auf der Startseite!
-</h1>
+<div class="row mt-4">
+    <div class="col-md-6 mb-3 mb-md-0">
+        <div class="card">
+            <div class="card-header">
+                <h6 class="display-6">AJAX-geladene Personentabelle</h6>
+            </div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                <h6 class="display-6">AJAX-geladene Personentabelle</h6>
+            </div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+</div>
