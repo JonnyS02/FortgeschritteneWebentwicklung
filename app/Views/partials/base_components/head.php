82673963aa2dd57a1ff26b0d1b1b28b0a10ca427
JonnyS02 <jona.stengl@gmail.com>
2025-08-08 12:26:16 +0200

    wetter

--- app/Views/partials/base_components/head.php
+++ app/Views/partials/base_components/head.php
@@ -13,9 +13,8 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
-    <!-- Favicon & eigene Styles -->
+    <!-- Favicon -->
     <link href="<?= base_url('kopf.png') ?>" type="image/x-icon" rel="icon">
-    <link href="<?= base_url('style.css') ?>" rel="stylesheet">
 
     <!-- jQuery -->
     <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
